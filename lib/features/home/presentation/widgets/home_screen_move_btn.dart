0dce731e41b54978a89764983f04d878bf2665d4
heui-yong <gusrnr9878@gmail.com>
2025-11-24 17:48:16 +0900

    [AIRE-12] : 홈 화면 및 이동 구현   - 각 버튼 UI 적용

--- lib/features/home/presentation/widgets/home_screen_move_btn.dart
+++ lib/features/home/presentation/widgets/home_screen_move_btn.dart
@@ -28,8 +28,37 @@ class _HomeScreenMoveBtnState extends ConsumerState<HomeScreenMoveBtn> {
       child: Container(
         height: 130,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(24),
-            gradient: widget.linearGradient
+          borderRadius: BorderRadius.circular(24),
+          gradient: widget.linearGradient,
+          boxShadow: [
+            BoxShadow(
+              color: AppColors.color_000000.withOpacity(0.25), 
+              blurRadius: 4,
+              offset: const Offset(4, 5),
+              spreadRadius: 0,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsetsGeometry.only(top: 5, left: 10, right: 22, bottom: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                widget.imagePath,
+                height: 120,
+                width: 120,
+              ),
+              Text(
+                widget.title,
+                style: const TextStyle(
+                  color: AppColors.color_ffffff,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ]
+          ),
         ),
       ),
     );
